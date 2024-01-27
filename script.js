6a30d110df33d9c9163e5a0548dcd0ec3fd71a78
Mulinti Vidhyanand Reddy <133974501+vidhyanandreddy@users.noreply.github.com>
2024-01-27 12:53:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,7 @@ function hideSidebar() {
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'none'
 }
+$(window).on('load',function(){
+    $(".loader").fadeOut(2000);
+    $("main,footer").fadeIn(6000);
+});
